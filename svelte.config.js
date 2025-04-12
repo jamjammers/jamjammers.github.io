d71b12f69a699c110c51669cc2a345d425fc0eca
jamjammers <jamesetam@gmail.com>
2025-04-12 17:58:03 -0500

    a

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,18 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+		  pages: 'build',
+		  assets: 'build',
+		  fallback: '404.html',
+		  precompress: false,
+		  strict: true
+		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/jamjammers.github.io' : '',
+		  base: process.env.NODE_ENV === 'production' ? '/jamjammers.github.io' : '',
 		}
-	}
+	  }
+	
 };
 
 export default config;
