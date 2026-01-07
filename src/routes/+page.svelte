e5e903da3aecde7eac183e4913df76075c9cb4dc
jamjammers <jamesetam@gmail.com>
2026-01-06 21:16:26 -0600

    Revert "dfrcc"
    This reverts commit 2616099a8ecfb5f98a3f53f7a706fe0e7770b744.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,1 @@
-<script>
-  import Game from 'Game.svelte';
-</script>
-
-<Game/>
-
-<br><br><br>
-<a href="UnityPlayer0.1.1.zip" download >Click me to install the Analysis Rhythm Game (windows)</a> <br>
-<br>
-<a href="UnityPlayer0.1.0.zip" style="font-size:10px;" download >old version (windows)</a> <br>
-<br>
-<span>In order to play the game, extract the zip file and open "AnalysisRhythmGame.exe"<br><br> 
-Mac OS and web coming soon!</span>
-<style>
-  a{
-    font-size: 30px;
-  }
-</style>
+Hi
