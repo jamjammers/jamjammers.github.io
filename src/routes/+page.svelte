2abb6c3ee61fc412d033a7e2b2e9485979261593
jamjammers <jamesetam@gmail.com>
2026-01-06 20:54:51 -0600

    format + hierarchy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,1 @@
-<script>
-  import Game from './Game.svelte';
-</script>
-<br>
-<a href="UnityPlayer0.1.1.zip" download >Click me to install the Analysis Rhythm Game (windows)</a> <br>
-<br>
-<a href="UnityPlayer0.1.0.zip" style="font-size:10px;" download >old version (windows)</a> <br>
-<br>
-<span>In order to play the game, extract the zip file and open "AnalysisRhythmGame.exe"<br>
-windows defender may say that this is a risky piece of software to run (most .exe files are), but you can run it anyways<br><br> 
-Mac OS and web coming soon!</span>
-<style>
-  a{
-    font-size: 30px;
-  }
-</style>
+Hi
---
+++ src/routes/AnalysisRhythmGame/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Game from '../Game.svelte';
+</script>
+
+<Game/>
+
+<br>
+<a href="UnityPlayer0.1.1.zip" download >Click me to install the Analysis Rhythm Game (windows)</a> <br>
+<br>
+<a href="UnityPlayer0.1.0.zip" style="font-size:10px;" download >old version (windows)</a> <br>
+<br>
+<span>In order to play the game, extract the zip file and open "AnalysisRhythmGame.exe"<br>
+windows defender may say that this is a risky piece of software to run (most .exe files are), but you can run it anyways<br><br> 
+Mac OS and web coming soon!</span>
+<style>
+  a{
+    font-size: 30px;
+  }
+</style>
